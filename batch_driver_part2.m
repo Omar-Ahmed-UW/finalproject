c62de16ee4ff4a374951e99b8f658c17ffb961f1
Omar Ahmed <oahmed@uw.edu>
2023-03-07 12:17:48 -0800

    batch driver

--- batch_driver_part2.m
+++ batch_driver_part2.m
@@ -1,4 +1,4 @@
-load('mnist.mat');
+T = readtable("train.csv", "NumHeaderLines", 1);
 network = BackpropNetwork(784, 100, 10);
 
 sumW1 = zeros(100, 784);
@@ -7,10 +7,10 @@ sumW2 = zeros(10, 100);
 sumB2 = zeros(10, 1);
 
 for k = 1:10
-    for i = 1:1200
+    for i = 1:960
         for j = 1:50
-            [network, ~] = network.networkForward(training.images(:, i*j));
-            [network, temp1, temp2] = network.networkSensitivity(training.labels(:, i*j));
+            [network, ~] = network.networkForward((T{i*j, 3:end})');
+            [network, temp1, temp2] = network.networkSensitivity(transformDigit(T{i*j, 2}));
             sumW1 = sumW1 + temp1*network.a0';
             sumB1 = sumB1 + temp1;
             sumW2 = sumW2 + temp2*network.a1';
@@ -26,9 +26,9 @@ for k = 1:10
 end
 
 count = 0;
-for m = 1:10000
-    [network, temp] = network.networkForward(test.images(:, m));
-    count = count + isequal(round(temp), test.labels(:, m));
+for m = 48001:60000
+    [network, temp] = network.networkForward(T{m, 3:end});
+    count = count + isequal(round(temp), transformDigit(T{m, 2}));
 end
 
 count / 100.00000
